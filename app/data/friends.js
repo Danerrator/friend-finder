3e4d1ffee2d11b273731ad053cb808441bf3ec5f
Dane Rugen <danerugen@gmail.com>
2019-09-26 17:38:12 -0700

    week13 Homework friend-finder

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+  {
+    name: "Jake Gyllenhaal",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Jake_Gyllenhaal_2019_by_Glenn_Francis.jpg",
+    //array of 10 numbers that will be sumed and compared to the sum of the user input
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Seth Rogen",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2012/11/takethiswaltz_cast_rogenseth.jpg",
+    scores: [4, 2, 1, 5, 3, 2, 1, 5, 2, 3]
+  },
+  {
+    name: "Ryan Gosling",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_.jpg",
+    scores: [2, 3, 1, 5, 2, 4, 3, 1, 5, 2]
+  },
+  {
+    name: "Margot Robbie",
+    photo: "https://gl-images.condecdn.net/image/oZMx0LwoMlN/crop/1020/f/margot-robbie-glamour-29sep16-getty-b.jpg",
+    scores: [4, 5, 2, 4, 3, 1, 5, 1, 3 , 4]
+  },
+  {
+    name: "Amber Heard",
+    photo: "https://s.yimg.com/ny/api/res/1.2/VXAi3QzriFGwUoaU4Eq96w--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-US/homerun/people_218/15c5edc30503decf4a18dde278314877",
+    scores: [3, 1, 5, 2, 3, 4, 1, 2, 3, 5, 4]
+  },
+  {
+    name: "Natalie Portman",
+    photo: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzU3MDYwMTEw/natalie-portman-9542326-1-402.jpg",
+    scores: [2, 3, 1, 5, 4, 2, 2, 3, 4, 1, 5]
+  }
+];
+
+module.exports = friends;
